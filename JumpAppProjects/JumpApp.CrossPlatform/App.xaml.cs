0e87f053de7519c28918e0c769eb7c6bc35e9a9f
RadoslawSt <41074648+RadoslawSt@users.noreply.github.com>
2021-01-25 14:25:55 +0000

    Update App.xaml.cs
    added line of code

--- JumpAppProjects/JumpApp.CrossPlatform/App.xaml.cs
+++ JumpAppProjects/JumpApp.CrossPlatform/App.xaml.cs
@@ -24,7 +24,8 @@ namespace JumpApp
             
             DependencyService.Register<IAzureRestService, AzureRestService>();
             DependencyService.Register<IRepositoryWrapper, RepositoryWrapper>();
-            DependencyService.Register<B2CAuthenticationService>();           
+            DependencyService.Register<B2CAuthenticationService>();
+            // Im adding this line of texxt
           
             Device.SetFlags(new[] {
                 "CarouselView_Experimental",
